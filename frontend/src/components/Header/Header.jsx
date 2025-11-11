08f507abf853c364eabd6a472064ab41a096a4c9
Rafael Pontes <rafapontes2008@hotmail.com>
2025-11-10 22:29:49 -0300

    feat: add styled Header with logo, wishlist, and user menu

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,5 +1,25 @@
+import styles from './Header.module.css';
+import { CircleUserRound, Heart } from 'lucide-react';
+
 export const Header = () => {
   return (
-    <div>Header</div>
+    <header className={styles.header}>
+      <img className={styles.logo} src="/logo.svg" alt="Logo" />
+      <div className={styles['header-options']}>
+        <div className={styles.wishlist}>
+          <Heart color="white" size={22} />
+          <span className={styles['wishlist-text']}>Wishlist</span>
+        </div>
+        <div className={styles['dropdown']} >
+          <CircleUserRound color="white" size={22} />
+          <div className={styles['dropdown-content']}>
+            <a href="#">Entrar</a>
+            <a href="#">Minha Conta</a>
+            <a href="#">Endereços</a>
+            <a href="#">Minha Netshoes</a>
+          </div>
+        </div>
+      </div>
+    </header>
   )
 }
