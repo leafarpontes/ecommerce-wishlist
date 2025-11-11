4c446eceeacc8868505e52d9001324279bfb3b10
Rafael Pontes <rafapontes2008@hotmail.com>
2025-11-11 20:44:27 -0300

    feat: enhance ProductCard with star rating and pricing display

--- frontend/src/components/ProductCard/ProductCard.jsx
+++ frontend/src/components/ProductCard/ProductCard.jsx
@@ -1,11 +1,38 @@
-import style from './ProductCard.module.css';
+import styles from './ProductCard.module.css';
+import { Star, StarHalf } from "lucide-react";
 
 export const ProductCard = ({ product }) => {
-  const { code, name, available, visible, details, priceInCents, salePriceInCents, rating, image, stockAvailable } = product;
+  const { name, priceInCents, salePriceInCents, rating, image } = product;
+
+  const fullStars = Math.floor(rating);
+  const halfStar = rating % 1 >= 0.5;
+
+  const displayPriceInBrl = (priceInCents) => {
+    return (priceInCents / 100).toFixed(2).replace('.', ',');
+  };
+
   return (
-    <div className={style.card}>
-      <img src={image} alt={name} className={style.image} />
-      <p className={style.description}>{name}</p>
+    <div className={styles.card}>
+      <img src={image} alt={name} className={styles.image} />
+      <p className={styles.description}>{name}</p>
+      <div className={styles['rating-container']}>
+        <div className={styles['star-rating']}>
+          <div className={styles.stars}>
+            {Array.from({ length: 5 }, () => (
+              <Star fill="#dbdbdc" strokeWidth={0} />
+            ))}
+          </div>
+          <div className={`${styles['stars']} ${styles['rating']}`}>
+            {Array.from({ length: fullStars }, () => (
+              <Star fill="#e79400" strokeWidth={0} />
+            ))}
+            {halfStar && <StarHalf fill="#e79400" strokeWidth={0} />}
+          </div>
+        </div>
+        <span className={styles['rating-number']}>{rating}</span>
+      </div>
+      <p className={styles.price}>R$ {displayPriceInBrl(priceInCents)}</p>
+      <p className={styles['sale-price']}>R$ {displayPriceInBrl(salePriceInCents)}</p>
     </div>
   )
 };
